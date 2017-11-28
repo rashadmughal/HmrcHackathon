677bf245d95640b7ad0059d20a5c86bc4dc9ee4e
Stew Abel <stew@grupettosoftware.com>
2017-11-28 12:33:52 +0000

    login posts and redirects

--- play-scala-seed/app/controllers/LoginController.scala
+++ play-scala-seed/app/controllers/LoginController.scala
@@ -1,5 +1,6 @@
 package controllers
 
+import java.lang.ProcessBuilder.Redirect
 import javax.inject._
 import play.api._
 import play.api.mvc._
@@ -7,5 +8,10 @@ import play.api.mvc._
 class LoginController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.ggLogin())
+  }
+
+  def submit() = Action { implicit request: Request[AnyContent] =>
+    //Ok(views.html.startPage())
+    Redirect(routes.HomeController.index())
   }
 }
